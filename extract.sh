74f1dfb1a8ed46a1b77c883ebe9595289d4b6c02
Tyson <343304907s@gmail.com>
2025-08-09 11:41:39 +0800

    first commit

--- extract.sh
+++ extract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python extract_feature.py --index 0 --imagenet_path "PATH TO ImageNet-1K"
+CUDA_VISIBLE_DEVICES=0 python extract_feature.py --index 0 --imagenet_path "PATH TO ImageNet-1K" #index分块处理数据 num_chunks=4
 
 wait
 echo "All processes completed"
